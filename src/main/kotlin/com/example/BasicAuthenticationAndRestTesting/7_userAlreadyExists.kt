51d337355c8708fc1deea06d2f29dce9e14e7bd5
faizzaidi23 <786faizzaidi@gmail.com>
2025-10-08 01:04:23 +0530

    a slight change in the message shown on the exception

--- src/main/kotlin/com/example/BasicAuthenticationAndRestTesting/7_userAlreadyExists.kt
+++ src/main/kotlin/com/example/BasicAuthenticationAndRestTesting/7_userAlreadyExists.kt
@@ -2,4 +2,4 @@ package com.example.BasicAuthenticationAndRestTesting
 
 import java.lang.RuntimeException
 
-class UserAlreadyExistsException(email: String): RuntimeException("User with this email `$email` already exists")
+class UserAlreadyExistsException(email: String): RuntimeException("User with email `$email` already exists")
